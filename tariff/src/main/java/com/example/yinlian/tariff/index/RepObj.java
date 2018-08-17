16349429d93d2887d4fa9984f8d272f5efe3bc18
luozhimin <838111691@qq.com>
2018-08-17 10:16:31 +0800

    添加获取设备sn和model的权限和代码222

--- tariff/src/main/java/com/example/yinlian/tariff/index/RepObj.java
+++ tariff/src/main/java/com/example/yinlian/tariff/index/RepObj.java
@@ -120,13 +120,12 @@ public class RepObj {
 
         String deviceInfoMap =null;  //获取sn
         String deviceFirmCode=null;  //获取model
-        Map<ModuleEnum,String> deviceInfo=null;
-        try {
+        /*  Map<ModuleEnum,String> deviceInfo=null;
+      try {
 //            deviceInfoMap = baseSystemManager.readSN().toString();
              deviceInfo =baseSystemManager.getDeviceInfo();
              //{model=APOS A8, is_support_script_print=1, sn=000001041743CA880954, is_common_sig=1, vendor=landi, is_support_wifiprobe=1, service_ver=1.0.27, is_support_rf_speedup=1, os_ver=5.0.7, is_support_scan_optimize=1, sdk_ver=2.0.1, is_support_chn_crypt_alg=1}
-            deviceInfoMap=android.os.Build.SERIAL;//sn
-            deviceFirmCode= android.os.Build.MODEL;// ：获取手机的型号 设备名称;
+
             KLog.d("REp",deviceInfoMap);
         } catch (SdkException e) {
             e.printStackTrace();
@@ -137,7 +136,10 @@ public class RepObj {
         }catch (NullPointerException e){
             KLog.d("REp","NullPointerException");
 
-        }
+        }*/
+        deviceInfoMap=android.os.Build.SERIAL;//sn
+        deviceFirmCode= android.os.Build.MODEL;// ：获取手机的型号 设备名称;
+
         deviceInfoJSon.setDeviceSn(deviceInfoMap);//终端硬件序列号
         deviceInfoJSon.setFirmCode(deviceFirmCode);//厂商代码
 
