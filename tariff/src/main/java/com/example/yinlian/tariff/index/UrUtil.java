566fa370d4e0ee222336af625b864b61ca9a75ff
luozhimin <838111691@qq.com>
2019-04-03 13:58:13 +0800

    计费接口的ip端口从39006改成39010

--- tariff/src/main/java/com/example/yinlian/tariff/index/UrUtil.java
+++ tariff/src/main/java/com/example/yinlian/tariff/index/UrUtil.java
@@ -5,7 +5,7 @@ package com.example.yinlian.tariff.index;
  */
 public class UrUtil {
 //    private final static String BASE="http://209a519.nat123.cc:13643/bmp/api/";
-    private final static String BASE="http://58.247.0.18:39006/bmp/api/";
+    private final static String BASE="http://58.247.0.18:39010/bmp/api/";
     //http://500995bc.nat123.cc:12986/bmp/api/
     public final static String findTariffInfoList =BASE+"findTariffInfoList";
     public final static String forTrial =BASE+"forTrial";
